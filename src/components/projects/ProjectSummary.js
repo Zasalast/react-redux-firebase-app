8de2220b846228636d5328eff063dcef48af1b33
foudilredaoui <foudil.redaoui@imadrassa.com>
2019-11-05 14:52:28 +0100

    Project Summary component

---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const ProjectSummary = ()=>{
+    return (
+            <div className="card z-depth-0 project-summary">
+                 <div className="card-content grey-text text-darken-3">
+                     <span className="card-title">Project Title </span>
+                     <p>Posted by Foudil REDAOUI</p>
+                     <p className="grey-text">5th Nov, 2pm</p>
+                 </div>
+            </div>
+    )
+}
+
+export default ProjectSummary;
