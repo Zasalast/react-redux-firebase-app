13e8be136c3887e6d32c20c8cf9b9567a2e13133
zasalastobon <zas_91@hotmail.com>
2022-08-15 11:19:11 -0500

    Update fbConfig.js

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -3,13 +3,13 @@ import 'firebase/firestore';
 import 'firebase/auth';
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyDcgkseYtbDSII17elZ8VRIURnhavdwZ10",
+    apiKey: "",
     authDomain: "marioplan-970ae.firebaseapp.com",
     databaseURL: "https://marioplan-970ae.firebaseio.com",
     projectId: "marioplan-970ae",
     storageBucket: "marioplan-970ae.appspot.com",
     messagingSenderId: "261334860919",
-    appId: "1:261334860919:web:e32f7c2ac889b26c25f272",
+    appId: "",
     measurementId: "G-GR0BYWC8CK"
   };
   // Initialize Firebase
